e084876f481164db4595d623530d75e5e72571b1
GUNN33RR <ghanemismail16@gmail.com>
2024-01-24 10:13:06 -0700

    cars

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,54 @@
 <template>
   <main>
-    <h1>FERNANDO BUTTCOX</h1>
+    <h1>KA-CHOW!</h1>
+    <p>[Inhaling and exhaling deeply]
+
+[male] Okay. Here we go. Focus.
+
+Speed. I am speed.
+
+[cars whizzing past]
+
+One winner, 42 losers.
+
+I eat losers for breakfast.
+
+[car accelerating]
+
+Breakfast.
+
+Wait, maybe I
+
+should have had breakfast.
+
+A little breck-y could be good for me.
+
+No, no, no, stay focused. Speed.
+
+[cars whizzing]
+
+I'm faster than fast.
+
+Quicker than quick.
+
+I am lightning!
+
+[pounding on door]
+
+[male] Hey, Lightning! You ready?
+
+[Sheryl Crow:
+Real Gone]
+
+Oh, yeah. Lightning's ready.
+
+[engine revving]
+
+[Engine revs]
+
+[Crowd cheers]
+
+Ka-chow!</p>
     <header>
       <nav>
       </nav>
